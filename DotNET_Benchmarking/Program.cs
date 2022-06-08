15d4ba544e90ef926087a6dc0f18cd8b95da4a32
johndward01 <johndward01@gmail.com>
2022-06-07 20:31:23 -0500

    Add project files.

---
+++ DotNET_Benchmarking/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace DotNET_Benchmarking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Md5VsSha256>();
+            Console.WriteLine(summary);
+        }
+    }
+}
